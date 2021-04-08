db7df12e9538777a8ad1f0b55c32185318df9b70
Jean Barcellos <jeanbarcellos@hotmail.com>
2021-04-07 23:58:46 -0300

    Configuração da injeção de dependências

--- src/TestEventStore.API/Startup.cs
+++ src/TestEventStore.API/Startup.cs
@@ -1,16 +1,11 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using TestEventStore.Core.Data.EventSourcing;
+using TestEventStore.EventSourcing;
 
 namespace TestEventStore.API
 {
@@ -23,9 +18,11 @@ namespace TestEventStore.API
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            // Event Sourcing
+            services.AddSingleton<IEventStoreService, EventStoreService>();
+            services.AddSingleton<IEventSourcingRepository, EventSourcingRepository>();
 
             services.AddControllers();
             services.AddSwaggerGen(c =>
@@ -34,7 +31,6 @@ namespace TestEventStore.API
             });
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
