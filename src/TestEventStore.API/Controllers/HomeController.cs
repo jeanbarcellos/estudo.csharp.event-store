5ee062197ab96d9fbccadea0d54167f7dea4e2ed
Jean Barcellos <jeanbarcellos@hotmail.com>
2021-04-07 23:59:21 -0300

    Realização dos testes no controller

--- src/TestEventStore.API/Controllers/HomeController.cs
+++ src/TestEventStore.API/Controllers/HomeController.cs
@@ -1,31 +1,63 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using System;
-using System.Text;
+using System.Collections.Generic;
 using System.Threading.Tasks;
-using EventStore.ClientAPI;
+using TestEventStore.API.Events;
+using TestEventStore.Core.Data.EventSourcing;
 
 namespace TestEventStore.API.Controllers
 {
     [ApiController]
-    [Route("[controller]")]
+    [Route("/events")]
     public class HomeController : ControllerBase
     {
-        [HttpGet]
-        public async Task<IActionResult> GetAsync()
+        private readonly IEventSourcingRepository _eventSourcingRepository;
+        private Guid _userId;
+
+
+        public HomeController(IEventSourcingRepository eventSourcingRepository)
         {
-            var conn = EventStoreConnection.Create(new Uri("tcp://admin:changeit@localhost:1113"));
-            await conn.ConnectAsync();
-
-            var data = Encoding.UTF8.GetBytes("{\"a\":\"2\"}");
-            var metadata = Encoding.UTF8.GetBytes("{}");
-            var evt = new EventData(Guid.NewGuid(), "testEvent", true, data, metadata);
-
-            await conn.AppendToStreamAsync("test-stream", ExpectedVersion.Any, evt);
-
-            var streamEvents = await conn.ReadStreamEventsForwardAsync("test-stream", 0, 1, false);
-            var returnedEvent = streamEvents.Events[0].Event;
-
-            return Ok($"Read event with data: {Encoding.UTF8.GetString(returnedEvent.Data)}, metadata: {Encoding.UTF8.GetString(returnedEvent.Metadata)}");
+            _eventSourcingRepository = eventSourcingRepository;
+            _userId = new Guid("28df745b-4127-4e89-8af1-9d6ea2cb260e");
         }
+
+        [HttpGet]
+        [Route("")]
+        public async Task<IActionResult> ListEventsAsync()
+        {
+            var events = await _eventSourcingRepository.GetEvents(_userId);
+
+            return Ok(events);
+        }
+
+        [HttpGet]
+        [Route("first")]
+        public async Task<IActionResult> ShowFirstEventAsync()
+        {
+            var events = await _eventSourcingRepository.GetFirstEvent(_userId);
+
+            return Ok(events);
+        }
+
+        [HttpGet]
+        [Route("last")]
+        public async Task<IActionResult> ShowLastEventAsync()
+        {
+            var events = await _eventSourcingRepository.GetLastEvent(_userId);
+
+            return Ok(events);
+        }
+
+        [HttpPost]
+        [Route("insert")]
+        public async Task<IActionResult> InsertEventAsync()
+        {
+            var eEvent = new UserCreatedEvent(_userId, "Jean Barcellos", "teste@teste");
+
+            await _eventSourcingRepository.SaveEvent(eEvent);
+
+            return Ok(eEvent);
+        }
+
     }
 }
---
+++ src/TestEventStore.API/Events/UserCreatedEvent.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using TestEventStore.Core.Messages;
+
+namespace TestEventStore.API.Events
+{
+    public class UserCreatedEvent : Event
+    {
+        public Guid UserId { get; private set; }
+        public string Name { get; private set; }
+        public string Email { get; private set; }
+
+        public UserCreatedEvent(Guid userId, string name, string email)
+        {
+            AggregateId = userId;
+            UserId = userId;
+            Name = name;
+            Email = email;
+        }
+    }
+}
