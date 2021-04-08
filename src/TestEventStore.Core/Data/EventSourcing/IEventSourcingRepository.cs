7ff039a5c5bc7a66cd8ab6ab38fb838805142a01
Jean Barcellos <jeanbarcellos@hotmail.com>
2021-04-07 22:48:36 -0300

    Implementação da interface IEventSourcingRepository

---
+++ src/TestEventStore.Core/Data/EventSourcing/IEventSourcingRepository.cs
@@ -0,0 +1,17 @@
+﻿using NerdStore.Core.Data.EventSourcing;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using TestEventStore.Core.Messages;
+
+namespace TestEventStore.Core.Data.EventSourcing
+{
+    public interface IEventSourcingRepository
+    {
+        public interface IEventSourcingRepository
+        {
+            Task SaveEvent<TEvent>(TEvent evento) where TEvent : Event;
+            Task<IEnumerable<StoredEvent>> GetEvents(Guid aggregateId);
+        }
+    }
+}
