2e031f66d7a3891a59cdddbd539bd3160779ba3d
Jean Barcellos <jeanbarcellos@hotmail.com>
2021-04-07 22:16:32 -0300

    Versão inicial conforme documentação

--- src/TestEventStore.API/Controllers/HomeController.cs
+++ src/TestEventStore.API/Controllers/HomeController.cs
@@ -1,4 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Text;
+using System.Threading.Tasks;
+using EventStore.ClientAPI;
 
 namespace TestEventStore.API.Controllers
 {
@@ -7,9 +11,21 @@ namespace TestEventStore.API.Controllers
     public class HomeController : ControllerBase
     {
         [HttpGet]
-        public IActionResult Get()
+        public async Task<IActionResult> GetAsync()
         {
-            return Ok("funcionou");
+            var conn = EventStoreConnection.Create(new Uri("tcp://admin:changeit@localhost:1113"));
+            await conn.ConnectAsync();
+
+            var data = Encoding.UTF8.GetBytes("{\"a\":\"2\"}");
+            var metadata = Encoding.UTF8.GetBytes("{}");
+            var evt = new EventData(Guid.NewGuid(), "testEvent", true, data, metadata);
+
+            await conn.AppendToStreamAsync("test-stream", ExpectedVersion.Any, evt);
+
+            var streamEvents = await conn.ReadStreamEventsForwardAsync("test-stream", 0, 1, false);
+            var returnedEvent = streamEvents.Events[0].Event;
+
+            return Ok($"Read event with data: {Encoding.UTF8.GetString(returnedEvent.Data)}, metadata: {Encoding.UTF8.GetString(returnedEvent.Metadata)}");
         }
     }
 }
