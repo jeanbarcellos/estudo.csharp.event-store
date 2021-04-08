31d579ff92e7a657c07d80bcea3bafce8f666aba
Jean Barcellos <jeanbarcellos@hotmail.com>
2021-04-07 21:42:48 -0300

    Initial

---
+++ src/TestEventStore.API/Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TestEventStore.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HomeController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("funcionou");
+        }
+    }
+}
