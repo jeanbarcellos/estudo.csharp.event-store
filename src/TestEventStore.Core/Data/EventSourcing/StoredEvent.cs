386acd4183696e049192db9718ac300f5867030b
Jean Barcellos <jeanbarcellos@hotmail.com>
2021-04-07 23:58:16 -0300

    Ajustes em TestEventStore.Core

---
+++ src/TestEventStore.Core/Data/EventSourcing/StoredEvent.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace TestEventStore.Core.Data.EventSourcing
+{
+    public class StoredEvent
+    {
+        public Guid Id { get; private set; }
+        public string Type { get; private set; }
+        public DateTime Timestamp { get; private set; }
+        public string Data { get; private set; }
+
+        public StoredEvent(Guid id, string type, DateTime timestamp, string data)
+        {
+            Id = id;
+            Type = type;
+            Timestamp = timestamp;
+            Data = data;
+        }
+    }
+}
